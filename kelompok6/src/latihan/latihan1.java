d0e63ff93cc07832a5b56453dcfbdef4e451a817
Ainur007 <115988539+Ainur007@users.noreply.github.com>
2022-10-18 12:38:30 +0700

    Add files via upload

---
+++ kelompok6/src/latihan/latihan1.java
@@ -0,0 +1,9 @@
+package latihan;
+
+public class latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Hellow");
+        System.out.println("Selamat Belajar di Program Java");
+        // TODO code application logic here
+    }
+}
